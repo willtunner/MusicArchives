65b1bb9de3a16b6c4160cb86d364db540c84b434
William <william@ampliato.com.br>
2024-04-26 14:14:29 -0300

    add variables to vercel

--- src/main/java/com/greencode/musicarchivebackend/config/CredentialsConfig.java
+++ src/main/java/com/greencode/musicarchivebackend/config/CredentialsConfig.java
@@ -5,17 +5,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:credentials.properties")
+//@PropertySource("classpath:credentials.properties")
 public class CredentialsConfig {
 
-    @Value("${cloud.aws.credentials.access-key}")
+//    @Value("${cloud.aws.credentials.access-key}")
+//    private String awsAccessKey;
+//
+//    @Value("${cloud.aws.credentials.secret-key}")
+//    private String awsSecretKey;
+//
+//    @Value("${spring.data.mongodb.uri}")
+//    private String mongoUri;
+
+    @Value("${AWS_ACCESS_KEY}")
     private String awsAccessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${AWS_SECRET_KEY}")
     private String awsSecretKey;
 
-    @Value("${spring.data.mongodb.uri}")
+    @Value("${MONGO_URI}")
     private String mongoUri;
+
 
 
     public String getAwsAccessKey() {
