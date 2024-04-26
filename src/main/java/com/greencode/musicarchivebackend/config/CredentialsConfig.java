7cbf19af5c1962899929f7582bfc62d127658f85
William <william@ampliato.com.br>
2024-04-26 14:11:23 -0300

    add security to env variables

---
+++ src/main/java/com/greencode/musicarchivebackend/config/CredentialsConfig.java
@@ -0,0 +1,32 @@
+package com.greencode.musicarchivebackend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:credentials.properties")
+public class CredentialsConfig {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String awsAccessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String awsSecretKey;
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+
+    public String getAwsAccessKey() {
+        return awsAccessKey;
+    }
+
+    public String getAwsSecretKey() {
+        return awsSecretKey;
+    }
+
+    public String getMongoUri() {
+        return mongoUri;
+    }
+}
